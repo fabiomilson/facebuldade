84cf3232387b92f61dc06a98dcdf1ea6d2d4d791
fnt.1299114999 <fnt.1299114999@L323>
2016-08-15 19:54:26 -0300

    First Commit

---
+++ Faculdade/src/main/java/info/packtudo/primeiraAula/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package info.packtudo.primeiraAula;
+
+/**
+ * @author Milson.Lima
+ *
+ */
+public class PrimeiroPrograma {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Meu primeiro programa");
+	}
+
+}
